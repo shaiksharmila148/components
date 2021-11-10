fbe002e8621cfdcdc0b32f0edd517dc0dc8a4564
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-10 11:53:09 +0530

    counter increment button click

--- my_app/src/components/Counter.js
+++ my_app/src/components/Counter.js
@@ -4,28 +4,44 @@ class Counter extends Component {
 
     constructor(props) {
         super(props)
-
         this.state = {
             count: 0
         }
     }
-
-    increase() {
-        this.setState({
+        increase() {
+        
+             /*  this.setState({ //setState method is async  --- executed on a different thread // it will re render the component
             count: this.state.count + 1
 
-        })   // it will re render the component
+    
+        },
+         () => { console.log('call back value ', this.state.count) })   //callback function(=>arrow function)  -- please execute this function[callback] only after the state has been set
+       
+        console.log(this.state.count)          //console is being written before setState method completes
+    */ 
+        this.setState((prevState,props) => ({
+            count: prevState.count + props.addValue
+        }))
         console.log(this.state.count)
+    }
+
+
+    increaseFive(){
+        this.increase()
+        this.increase()
+        this.increase()
+        this.increase()
+        this.increase()
     }
 
     render() {
         return (
             <div>
                 Count - {this.state.count}
-                <button onClick={() => this.increase()}>increment</button>
+                
+                <button onClick={() => this.increaseFive()}>increment</button>
             </div>
         )
     }
 }
-
 export default Counter
