7db7c2139584234fd0cbb39b6fd3d30bb871c472
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-10 11:15:28 +0530

    componenets

--- my_app/src/components/Greet.js
+++ my_app/src/components/Greet.js
@@ -1,8 +1,11 @@
 import React from  'react'
 
-function Greet(){
+    /* function Greet(){
     return  <h1>welcome Lucky :)</h1>
-}
+
+} */
+
+const Greet = () => <h1>hello lucky</h1>
 
 export default Greet 
 
