d3329524275538050fe0fe6fee425892a5f3fe18
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-10 12:27:41 +0530

    destructing in stateful component

--- my_app/src/components/Welcome.js
+++ my_app/src/components/Welcome.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 class Welcome extends Component{
 
     render(){
-        return <h1>welcome mr ravi kumar</h1>
+        const {name,alias} = this.props
+        const{state1,state2} = this.state
+        return <h1>welcome mr {name} your alias is {alias}</h1>
     }
 
 }
 
-export default Welcome 
+export default Welcome
