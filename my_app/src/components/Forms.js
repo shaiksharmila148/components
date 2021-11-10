99ffe6e17a2206ca54d9aa7f8349bf339994d31c
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-10 15:28:08 +0530

    select component

--- my_app/src/components/Forms.js
+++ my_app/src/components/Forms.js
@@ -1,37 +1,59 @@
-import React, { Component } from 'react'
+class Form extends Component {
 
-class Forms extends Component {
-
-constructor(props) {
-    super(props)
-
-    this.state = {
-         username: '',
-         comments: ''
+    
+        constructor(props) {
+            super(props)
+    
+    
+            this.state = {
+                username: '',
+                comments: '',
+                topic: 'react'
+            }
+        }
+    
+    
+    
+        handleUsernameChange = (event) => {
+     constructor(props) {
+            })
+        }
+    
+        handleCommentsChange = (event) => {
+            this.setState({
+                comments: event.target.value
+            })
+        }
+    
+        handleTopicChange = (event) => {
+            this.setState({
+                topic: event.target.value
+            })
+        }
+    
+        render() {
+            return (
+                <form>
+                    <div>
+                        <label>username</label>
+                        <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
+                    </div>
+                    <div>
+                        <label>comments</label>
+                        <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                    </div>
+                    <div>
+                        <label>topics</label>
+                        <select value = {this.state.topic} onChange = {this.handleTopicChange}>
+                            <option value="react">React</option>
+                            <option value="angular">Angular</option>
+                            <option value="vue">Vue</option>
+    
+                        </select>
+                    </div>
+                </form>
+            )
+        }
     }
-}
-
-
-    handleUsernameChange = (event) => {
-        this.setState({
-            username: event.target.value
-        })
-    }
-
-    render() {
-        return (
-            <forms>
-                <div>
-                    <label>username</label>
-                    <input type='text' value ={this.state.username} onChange = {this.handleUsernameChange} />
-                </div>
-                <div>
-                    <label>comments</label>
-                    <textarea value = {this.state.comments }></textarea>
-                </div>
-            </forms>
-        )
-    }
-}
-
-export default Forms
+    export default Form
+    
