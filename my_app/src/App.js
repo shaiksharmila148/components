682c8fcf24cf514b91f2c7bdd5455b5ea2500f09
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-10 11:35:21 +0530

    handling state change

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -2,19 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
+
+
 
 
 function App() {
   return (
     <div className="App">
+      <Message/>
   
       <Greet name = "lucky"  college = "IIT">
       <p> this is children property</p>
     </Greet>
-    <Greet name = "ravi" college = "Oxford"/>
-    <Greet name ="kumar"/>
+    <Greet name = "ansari" college = "Oxford">
+      <button>click me</button>
+    </Greet>
+    <Greet />
 
-    <Welcome/>
+    <Welcome name ="tanveer" alias = "ansari"/>
 
     </div>
   );
---
+++ my_app/src/components/Message.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Message extends Component {
+    constructor() {
+        super()
+        this.state = {
+            message: 'welcome visitor to dxc'  //message partition
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button   onClick = {()=> this.changeMessage()}>subscribe</button>
+
+            </div>
+        )
+    }
+
+    changeMessage(){
+        this.setState({    //changing the content of the partition
+            message: 'thank you for subscribing'
+        })
+    }
+
+}
+
+export default Message 
