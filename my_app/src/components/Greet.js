fcc17aafdce702970b19f5ac592a76f18a57cbc3
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-10 11:27:14 +0530

    welcom.js

--- my_app/src/components/Greet.js
+++ my_app/src/components/Greet.js
@@ -1,11 +1,21 @@
-import React from  'react'
+
+import React from 'react'
 
     /* function Greet(){
     return  <h1>welcome Lucky :)</h1>
 
 } */
 
-const Greet = () => <h1>hello lucky</h1>
+const Greet = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1> hello  {props.name} you are from {props.college}</h1>
+            {props.children}
+        </div>
+    )
+
+}
 
 export default Greet 
 
