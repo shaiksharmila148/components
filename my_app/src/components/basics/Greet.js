2cba49e4b7a028224395672dc05d0c7dadb39de3
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-12 15:24:52 +0530

    refs demo

---
+++ my_app/src/components/basics/Greet.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+/* function Greet(){
+    return  <h1>welcome lucky</h1>
+} */
+
+//const Greet = ({name,college}) => {
+    const Greet = props => {
+        const {name,college} = props
+    //console.log(props);
+    //props.name = 'dxc'
+    return (
+        <div>
+            <h1> hello  {name} you are from {college}</h1>
+        </div>
+    )
+
+}
+export default Greet
---
+++ my_app/src/components/basics/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Welcome extends Component{
+
+    render(){
+        const {name,alias} = this.props
+       // const{state1,state2} = this.state
+        return <h1>welcome mr {name} your alias is {alias}</h1>
+    }
+
+}
+
+export default Welcome
