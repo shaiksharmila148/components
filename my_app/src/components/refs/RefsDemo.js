0af11741e74030b2de25a039e23e07a4e56dfb02
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-12 15:30:16 +0530

    refs in class components -- focus a child from a parent

---
+++ my_app/src/components/refs/RefsDemo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+/**
+ * refs is about getting the handle of  dom element
+ */
+ class RefsDemo extends Component {
+    constructor(props) {
+        super(props)
+    
+    this.inputRef = React.createRef()
+    }
+    
+    componentDidMount(){
+       this.inputRef.current.focus()
+        console.log(this.inputRef)
+    }
+
+clickHandler = () => {
+    alert(this.inputRef.current.value)
+}
+
+    render() {
+        return (
+            <div>
+                <input type = "text" ref = {this.inputRef}/>
+                <button onClick = {this.clickHandler}> fetch value</button>
+            </div>
+        )
+    }
+}
+
+export default RefsDemo
