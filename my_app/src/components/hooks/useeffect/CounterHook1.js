4edb07aeb1f4ef7cdb6f89cfa95f9d4858a32dc1
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-17 15:40:48 +0530

    useEffect conditional run in functional component

--- my_app/src/components/hooks/useeffect/CounterHook1.js
+++ my_app/src/components/hooks/useeffect/CounterHook1.js
@@ -2,15 +2,28 @@ import React, { useState,useEffect } from 'react'
 
 function CounterHook1() {
 const [count,setCount] = useState(0)
+const [name,setName] = useState('')
 //use effect will be called after render method executes each time
 useEffect(
-    ()=>{document.title = `you clicked ${count} times`}
+    ()=>{
+        console.log('updating -- useEffect hook')
+        document.title = `you clicked ${count} times`
+    },
+    [count] //hey update only when the count variable changes
 )
     return (
         <div>
+            <input
+                type ='text'
+                value = {name}
+                onChange = {
+                    e => setName(e.target.value)
+                }
+            />
             <button onClick = { ()=> setCount(count+1)}>Click {count} times</button>
         </div>
     )
 }
 
 export default CounterHook1
+
