0d46549d089ef88c54e4da3fdeda240574efb8d2
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-17 16:53:28 +0530

    useEffect componentWillUnmount window.removeEventListener

---
+++ my_app/src/components/hooks/useeffect/ClassMouse.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+//run effects only once
+export class ClassMouse extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             x:0,
+             y:0
+        }
+    }
+
+    logMousePosition = e =>{
+        this.setState({
+            x: e.clientX, y: e.clientY
+        })
+    }
+    
+componentDidMount(){
+    window.addEventListener('mousemove',this.logMousePosition) //i am adding this listener only once
+}
+
+    componentWillUnmount(){
+        window.removeEventListener('mousemove',this.logMousePosition)
+    }
+
+    render() {
+        return (
+            <div>
+                X - {this.state.x} Y- {this.state.y}
+            </div>
+        )
+    }
+}
+
+export default ClassMouse
